7b3f41787434f990cdd3529c8b60437112e39bfc
Brigita Maria Wiputri <bmwiputri@gmail.com>
2022-05-02 10:18:53 +0700

    feat: implement InputAutocomplete to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import InputAutocomplete from './ui/components/InputAutocomplete';
+import { MappedEmployeeWithEmail } from './ui/types';
+import { getEmployees } from './datasource';
+import { mapEmployeesWithAccountEmail } from './helpers/mapEmployeesWithAccountEmail';
+
+import './App.scss';
 
 function App() {
+  const [employees, setEmployees] = useState<MappedEmployeeWithEmail[]>([])
+
+  useEffect(() => {
+    getEmployees().then((res) => {
+      setEmployees(mapEmployeesWithAccountEmail(res))
+    });
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputAutocomplete data={employees} />
     </div>
   );
 }
